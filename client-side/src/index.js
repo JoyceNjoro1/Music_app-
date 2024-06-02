beda1bc32ab6ee3fd2c978b3ddab7a9d1b5add7c
Joyce Njoroge <136052595+JoyceNjoro1@users.noreply.github.com>
2024-06-02 21:04:34 +0300

    spotify react app

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from 'redux-thunk';
 import { Provider } from "react-redux";
 import reducers from "./reducers";
 import App from "./App";
